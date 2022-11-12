4a97da475a9474086a403afddd2c471e399c08ad
Konrad Miziński <gmanos@interia.pl>
2022-11-12 05:42:47 +0100

    Fix metody toString

--- projekt-stats/src/main/java/pwo/projekt/stats/utils/FileType.java
+++ projekt-stats/src/main/java/pwo/projekt/stats/utils/FileType.java
@@ -22,6 +22,6 @@ public enum FileType {
      */
     @Override
     public String toString() {
-        return this.toString().toLowerCase();
+        return super.toString().toLowerCase();
     }
 }
